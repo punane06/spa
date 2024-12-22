4fbd2562f93a1cf8b729bc1e08ae7f705e1b3076
Kadi Kerner <kadikerner@gmail.com>
2024-12-22 23:56:12 +0200

    fix table lodaing

--- app/list/loading.tsx
+++ app/list/loading.tsx
@@ -2,9 +2,10 @@
 import Image from "next/image";
 
 const Loading = () => {
-    return 
-    <div className="h-full w-full flex justify-center items-center">
-        <Image className="a" src="/loader.svg" alt={"loading"} width={0} height={0} style={{width: '10%', height: 'auto'}} />
-    </div>
+    return (
+        <div className="h-full w-full flex justify-center items-center">
+            <Image className="a" src="/loader.svg" alt={"loading"} width={0} height={0} style={{width: '10%', height: 'auto'}} />
+        </div>
+    );
 };
 export default Loading;
--- app/list/page.tsx
+++ app/list/page.tsx
@@ -4,6 +4,7 @@ import { H1 } from "../components/ui/HeadingProps";
 import { ContentContainer } from "../components/ui/ContainerProps";
 import { Table, Td, Th, THead, TrBody, TrHead } from '../components/ui/TableProps';
 import Image from 'next/image'
+import Loading from "./loading";
 
 export interface DataItem {
     id: string;
@@ -18,6 +19,7 @@ const ITEMS_PER_PAGE = 10;
 const MAX_ITEMS = 108;
 
 const TableComponent = () => {
+    const [loading, setLoading] = useState(true);
     const [data, setData] = useState<DataItem[]>([]);
     const [defaultData, setDefaultData] = useState<DataItem[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
@@ -28,11 +30,13 @@ const TableComponent = () => {
 
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true);
             const response = await fetch('https://proovitoo.twn.ee/api/list');
             const result = await response.json() as { list: DataItem[] };
             const limitedData = result.list.slice(0, MAX_ITEMS);
             setData(limitedData);
             setDefaultData(limitedData);
+            setLoading(false);
         };
 
         fetchData();
@@ -178,91 +182,94 @@ const TableComponent = () => {
 
     return (
         <ContentContainer>
-            <H1 className="text-center mx-auto mb-[24px] md:mb-[30px] ">Nimekiri</H1>
-
-            <div className="block overflow-auto w-full">
-                <Table>
-                    <THead>
-                        <TrHead>
-                            <Th onClick={handleFirstnameSort} className="cursor-pointer">
-                                <div className="flex items-center">
-                                    <span className="mr-1">
-                                        Eesnimi
-                                    </span>
-                                    <div className="relative bg-red-300 h-full w-full">
-                                        <Image className={`absolute -bottom-1 rotate-0 ${firstnameSortOrder === 1 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
-                                        <Image className={`absolute -top-1 rotate-180 ${firstnameSortOrder === 2 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
-                                    </div>
-                                </div>
-                            </Th>
-                            <Th onClick={handleSurnameSort} className="cursor-pointer">
-                                <div className="flex items-center">
-                                    <span className="mr-1">Perekonnanimi</span>
-                                    <div className="relative bg-red-300 h-full w-full">
-                                        <Image className={`absolute -bottom-1 rotate-0 ${surnameSortOrder === 1 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
-                                        <Image className={`absolute -top-1 rotate-180 ${surnameSortOrder === 2 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
-                                    </div>
-                                </div>
-                            </Th>
-                            <Th onClick={handleSexSort} className="cursor-pointer">
-                                <div className="flex items-center">
-                                    <span className="mr-1">Sugu</span>
-                                    <div className="relative bg-red-300 h-full w-full">
-                                        <Image className={`absolute -bottom-1 rotate-0 ${sexSortOrder === 1 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
-                                        <Image className={`absolute -top-1 rotate-180 ${sexSortOrder === 2 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
-                                    </div>
-                                </div>
-                            </Th>
-                            <Th onClick={handleBirthdaySort} className="cursor-pointer">
-                                <div className="flex items-center">
-                                    <span className="mr-1">Sünnikuupäev</span>
-                                    <div className="relative bg-red-300 h-full w-full">
-                                        <Image className={`absolute -bottom-1 rotate-0 ${birthdaySortOrder === 1 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
-                                        <Image className={`absolute -top-1 rotate-180 ${birthdaySortOrder === 2 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
-                                    </div>
-                                </div>
-                            </Th>
-                            <Th>Telefon</Th>
-                        </TrHead>
-                    </THead>
-                    <tbody>
-                        {displayedData.map((item) => (
-                            <TrBody key={item.id}>
-                                <Td>{item.firstname}</Td>
-                                <Td>{item.surname}</Td>
-                                <Td>{item.sex === 'm' ? 'Mees' : 'Naine'}</Td>
-                                <Td>{extractBirthday(parseInt(item.personal_code))}</Td>
-                                <Td>{item.phone}</Td>
-                            </TrBody>
+            {loading ? <Loading />	 : (
+                <>
+                    <H1 className="text-center mx-auto mb-[24px] md:mb-[30px] ">Nimekiri</H1>
+                    <div className="block overflow-auto w-full">
+                        <Table>
+                            <THead>
+                                <TrHead>
+                                    <Th onClick={handleFirstnameSort} className="cursor-pointer">
+                                        <div className="flex items-center">
+                                            <span className="mr-1">
+                                                Eesnimi
+                                            </span>
+                                            <div className="relative bg-red-300 h-full w-full">
+                                                <Image className={`absolute -bottom-1 rotate-0 ${firstnameSortOrder === 1 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
+                                                <Image className={`absolute -top-1 rotate-180 ${firstnameSortOrder === 2 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
+                                            </div>
+                                        </div>
+                                    </Th>
+                                    <Th onClick={handleSurnameSort} className="cursor-pointer">
+                                        <div className="flex items-center">
+                                            <span className="mr-1">Perekonnanimi</span>
+                                            <div className="relative bg-red-300 h-full w-full">
+                                                <Image className={`absolute -bottom-1 rotate-0 ${surnameSortOrder === 1 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
+                                                <Image className={`absolute -top-1 rotate-180 ${surnameSortOrder === 2 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
+                                            </div>
+                                        </div>
+                                    </Th>
+                                    <Th onClick={handleSexSort} className="cursor-pointer">
+                                        <div className="flex items-center">
+                                            <span className="mr-1">Sugu</span>
+                                            <div className="relative bg-red-300 h-full w-full">
+                                                <Image className={`absolute -bottom-1 rotate-0 ${sexSortOrder === 1 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
+                                                <Image className={`absolute -top-1 rotate-180 ${sexSortOrder === 2 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
+                                            </div>
+                                        </div>
+                                    </Th>
+                                    <Th onClick={handleBirthdaySort} className="cursor-pointer">
+                                        <div className="flex items-center">
+                                            <span className="mr-1">Sünnikuupäev</span>
+                                            <div className="relative bg-red-300 h-full w-full">
+                                                <Image className={`absolute -bottom-1 rotate-0 ${birthdaySortOrder === 1 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
+                                                <Image className={`absolute -top-1 rotate-180 ${birthdaySortOrder === 2 ? 'opacity-0' : ''}`} src='/arrow-filled.svg' alt="sort" width={20} height={20} priority />
+                                            </div>
+                                        </div>
+                                    </Th>
+                                    <Th>Telefon</Th>
+                                </TrHead>
+                            </THead>
+                            <tbody>
+                                {displayedData.map((item) => (
+                                    <TrBody key={item.id}>
+                                        <Td>{item.firstname}</Td>
+                                        <Td>{item.surname}</Td>
+                                        <Td>{item.sex === 'm' ? 'Mees' : 'Naine'}</Td>
+                                        <Td>{extractBirthday(parseInt(item.personal_code))}</Td>
+                                        <Td>{item.phone}</Td>
+                                    </TrBody>
+                                ))}
+                            </tbody>
+                        </Table>
+                    </div>
+                    <div className="flex justify-center items-center mt-[20px] font-bold">
+                        <button 
+                            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))} 
+                            disabled={currentPage === 1}
+                            className={`w-8 h-8 ${currentPage === 1 ? 'opacity-40 cursor-auto' : 'cursor-pointer hover:rounded-[2.5rem] hover:border hover:border-white'}`} 
+                        >
+                            <Image className='h-full my-0 mx-auto text-clip w-auto p-1' src='/arrow.svg' alt="prev" width={150} height={250} priority />
+                        </button>
+                        {getPaginationRange(currentPage, totalPages).map((page) => (
+                            <button 
+                                key={page} 
+                                onClick={() => setCurrentPage(page)} 
+                                className={`w-8 h-8 ml-4 ${currentPage === page ? 'bg-white text-[#333333] rounded-[2.5rem]' : 'hover:rounded-[2.5rem] hover:border hover:border-white'} `}
+                            >
+                                {page}
+                            </button>
                         ))}
-                    </tbody>
-                </Table>
-            </div>
-            <div className="flex justify-center items-center mt-[20px] font-bold">
-                <button 
-                    onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))} 
-                    disabled={currentPage === 1}
-                    className={`w-8 h-8 ${currentPage === 1 ? 'opacity-40 cursor-auto' : 'cursor-pointer hover:rounded-[2.5rem] hover:border hover:border-white'}`} 
-                >
-                    <Image className='h-full my-0 mx-auto text-clip w-auto p-1' src='/arrow.svg' alt="prev" width={150} height={250} priority />
-                </button>
-                {getPaginationRange(currentPage, totalPages).map((page) => (
-                    <button 
-                        key={page} 
-                        onClick={() => setCurrentPage(page)} 
-                        className={`w-8 h-8 ml-4 ${currentPage === page ? 'bg-white text-[#333333] rounded-[2.5rem]' : 'hover:rounded-[2.5rem] hover:border hover:border-white'} `}
-                    >
-                        {page}
-                    </button>
-                ))}
-                <button 
-                    onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))} 
-                    disabled={currentPage === totalPages}
-                    className={`w-8 h-8 ml-4 ${currentPage === totalPages ? 'opacity-40 cursor-auto' : 'cursor-pointer hover:rounded-[2.5rem] hover:border hover:border-white'}`} 
-                >
-                    <Image className='h-full my-0 mx-auto text-clip w-auto p-1 rotate-180' src='/arrow.svg' alt="next" width={150} height={250} priority />
-                </button>
-            </div>
+                        <button 
+                            onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))} 
+                            disabled={currentPage === totalPages}
+                            className={`w-8 h-8 ml-4 ${currentPage === totalPages ? 'opacity-40 cursor-auto' : 'cursor-pointer hover:rounded-[2.5rem] hover:border hover:border-white'}`} 
+                        >
+                            <Image className='h-full my-0 mx-auto text-clip w-auto p-1 rotate-180' src='/arrow.svg' alt="next" width={150} height={250} priority />
+                        </button>
+                    </div>
+                </>
+            )}
         </ContentContainer>
     );
 };
